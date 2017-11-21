3c4e0064c566d5fd57c3803cee17fc0e120b675a
Chris Loer <chris.loer@gmail.com>
2017-11-21 09:37:17 -0800

    Add acknowledgment comment to tiny_sdf.hpp

--- src/mbgl/util/tiny_sdf.hpp
+++ src/mbgl/util/tiny_sdf.hpp
@@ -5,6 +5,13 @@
 namespace mbgl {
 namespace util {
 
+/*
+    C++ port of https://github.com/mapbox/tiny-sdf, which is in turn based on the
+    Felzenszwalb/Huttenlocher distance transform (https://cs.brown.edu/~pff/dt/)
+ 
+    Takes an alpha channel raster input and transforms it into an alpha channel
+    Signed Distance Field (SDF) output of the same dimensions.
+*/
 AlphaImage generateTinySDF(const AlphaImage& rasterInput, double radius, double cutoff);
 
 } // namespace util
